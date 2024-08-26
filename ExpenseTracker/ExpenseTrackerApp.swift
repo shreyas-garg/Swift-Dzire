0065a93c3947e0d28f4a1bb7e44e54fdb0d43808
Shreyas-hub-coder <shreyas.garg2005@gmail.com>
2024-08-27 00:30:12 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Shreyas Garg on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Shreyas Garg on 27/08/24.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
